5ce041f2dbe919373af3798814b599ab98c5f601
Michael Cuffaro <consulting@michaelcuffaro.com>
2022-03-18 15:08:37 -0400

    change table names and add foreign key dependency

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,50 @@ use futures::TryStreamExt;
 
 #[async_std::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let pool = SqlitePoolOptions::new().max_connections(5).connect("cmi-pb.db").await?;
+    let pool = SqlitePoolOptions::new().max_connections(5).connect("sqlite://cmi-pb.db").await?;
+
     sqlx::query("PRAGMA foreign_keys = ON").execute(&pool).await?;
 
-    let mut rows = sqlx::query("SELECT * FROM foobar WHERE child LIKE ?").bind("%").fetch(&pool);
+    sqlx::query("DROP TABLE IF EXISTS `bar`").execute(&pool).await?;
+    sqlx::query(
+        format!(
+            r#"CREATE TABLE `bar` (
+                             `parent` TEXT,
+                             `child` TEXT,
+                             FOREIGN KEY (`child`) REFERENCES `foo`(`child`)
+                           )"#
+        )
+        .as_str(),
+    )
+    .execute(&pool)
+    .await?;
+
+    sqlx::query("DROP TABLE IF EXISTS `foo`").execute(&pool).await?;
+    sqlx::query(
+        format!(
+            r#"CREATE TABLE `foo` (
+                             `child` TEXT PRIMARY KEY
+                           )"#
+        )
+        .as_str(),
+    )
+    .execute(&pool)
+    .await?;
+
+    sqlx::query("INSERT INTO `foo` (`child`) VALUES (?), (?)")
+        .bind("a")
+        .bind("b")
+        .execute(&pool)
+        .await?;
+    sqlx::query("INSERT INTO `bar` (`parent`, `child`) VALUES (?, ?), (?, ?)")
+        .bind("x")
+        .bind("a")
+        .bind("y")
+        .bind("b")
+        .execute(&pool)
+        .await?;
+
+    let mut rows = sqlx::query("SELECT * FROM `bar` WHERE `child` LIKE ?").bind("%").fetch(&pool);
 
     while let Some(row) = rows.try_next().await? {
         // map the row into a user-defined domain type
@@ -19,7 +59,7 @@ async fn main() -> Result<(), sqlx::Error> {
     println!("---------------------");
 
     let rows_all =
-        sqlx::query("SELECT * FROM foobar WHERE child LIKE ?").bind("%").fetch_all(&pool).await?;
+        sqlx::query("SELECT * FROM `bar` WHERE `child` LIKE ?").bind("%").fetch_all(&pool).await?;
 
     let second_row = &rows_all[1];
     let parent: &str = second_row.try_get("parent")?;
@@ -27,7 +67,7 @@ async fn main() -> Result<(), sqlx::Error> {
 
     println!("---------------------");
 
-    let maybe_a_row = sqlx::query("SELECT * FROM foobar WHERE CHILD LIKE ?")
+    let maybe_a_row = sqlx::query("SELECT * FROM `bar` WHERE `child` LIKE ?")
         .bind("z")
         .fetch_optional(&pool)
         .await?;
@@ -43,7 +83,7 @@ async fn main() -> Result<(), sqlx::Error> {
     println!("---------------------");
 
     let definitely_a_row =
-        sqlx::query("SELECT * FROM foobar WHERE CHILD LIKE ?").bind("a").fetch_one(&pool).await?;
+        sqlx::query("SELECT * FROM `bar` WHERE `child` LIKE ?").bind("a").fetch_one(&pool).await?;
 
     let parent: &str = definitely_a_row.try_get("parent")?;
     println!("FETCH ONE {:?}", parent);
